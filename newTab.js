07a8190c89aacaca622ca1a014d30aef6c991514
Connor Kerns <connor-k@users.noreply.github.com>
2016-06-24 19:00:02 -0700

    Display all the components without formatting

--- newTab.js
+++ newTab.js
@@ -12,6 +12,20 @@ function getFact() {
   xhr.send();
 }
 
+function getImage() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      resp = xhr.responseXML;
+      document.body.style.background = "url('" + resp.getElementsByTagName("url")[0].innerHTML + "') repeat 50% 50%";
+      document.getElementById("catImageAttribution").href = resp.getElementsByTagName("source_url")[0].innerHTML;
+    }
+  }
+  xhr.open("GET", "https://thecatapi.com/api/images/get?format=xml&results_per_page=1", true);
+  xhr.send();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   getFact();
+  getImage();
 });
